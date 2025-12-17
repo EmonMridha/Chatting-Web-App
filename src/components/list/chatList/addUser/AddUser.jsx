92f99081e56fe41b9640e108f6eb3cb830a6b59a
EmonMridha <emonmridha712@gmail.com>
2025-12-17 18:41:26 +0600

    added key

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,11 +1,13 @@
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { arrayUnion, collection, doc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore';
 import './addUser.css'
 import { db } from '../../../../lib/firebase';
 import { useState } from 'react';
+import { useUserStore } from '../../../../lib/userStore';
 
 const AddUser = () => {
 
     const [user, setUser] = useState(null);
+    const { currentUser } = useUserStore();
 
     const handleSearch = async (e) => {
         e.preventDefault();
@@ -26,6 +28,48 @@ const AddUser = () => {
             console.log(err);
         }
     }
+
+    const handleAdd = async () => {
+
+        const chatRef = collection(db, 'chats')
+        const userChatsRef = collection(db, 'userchats')
+
+        try {
+
+            const newChatRef = doc(chatRef)
+
+            // Creating a new chat room
+            await setDoc(newChatRef, {
+                createdAt: serverTimestamp(),
+                messages: [],
+            });
+
+            // Updating the other user's userchats
+            await updateDoc(doc(userChatsRef, user.id), {
+                chats: arrayUnion({
+                    chatId: newChatRef.id,
+                    lastMessage: '',
+                    receiverId: currentUser.id,
+                    updatedAt: Date.now(),
+                })
+            })
+
+            // Updating my userchats
+            await updateDoc(doc(userChatsRef, currentUser.id), {
+                chats: arrayUnion({
+                    chatId: newChatRef.id,
+                    lastMessage: '',
+                    receiverId: user.id,
+                    updatedAt: Date.now(),
+                })
+            })
+
+            console.log(newChatRef.id);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
     return (
         <div className='addUser'>
             <form onSubmit={handleSearch}>
@@ -37,7 +81,7 @@ const AddUser = () => {
                     <img src="./avatar.png" alt="" />
                     <span>{user.username}</span>
                 </div>
-                <button>Add User</button>
+                <button onClick={handleAdd}>Add User</button>
             </div>}
 
         </div>
